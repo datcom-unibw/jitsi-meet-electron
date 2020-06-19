912bf75887983a2f5f0df162af5c9dad34c0a786
datcom-unibw <feedback@datcom-unibw.de>
2020-06-19 21:04:52 +0200

    Apply DatCom branding to app/features/config/index.js

--- app/features/config/index.js
+++ app/features/config/index.js
@@ -8,17 +8,17 @@ export default {
     /**
      * The URL to the source code repository.
      */
-    sourceURL: 'https://github.com/jitsi/jitsi-meet-electron',
+    sourceURL: 'https://github.com/datcom-unibw/jitsi-meet-electron',
 
     /**
      * Application name.
      */
-    appName: 'Jitsi Meet',
+    appName: 'DatCom Jitsi Meet',
 
     /**
      * The default server URL of Jitsi Meet Deployment that will be used.
      */
-    defaultServerURL: 'https://meet.jit.si',
+    defaultServerURL: 'https://meet2.datcom-unibw.de',
 
     /**
      * The default server Timeout in seconds.
@@ -28,15 +28,15 @@ export default {
     /**
      * URL to send feedback.
      */
-    feedbackURL: 'mailto:support@jitsi.org',
+    feedbackURL: 'mailto:feedback@datcom-unibw.de',
 
     /**
      * The URL of Privacy Policy Page.
      */
-    privacyPolicyURL: 'https://jitsi.org/meet/privacy',
+    privacyPolicyURL: 'https://meet2.datcom-unibw.de/privacy.html',
 
     /**
      * The URL of Terms and Conditions Page.
      */
-    termsAndConditionsURL: 'https://jitsi.org/meet/terms'
+    termsAndConditionsURL: 'https://meet2.datcom-unibw.de/terms.html'
 };
