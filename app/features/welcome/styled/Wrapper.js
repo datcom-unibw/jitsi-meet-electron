a13697e8ac08da99ecd38be32d36de36367c3e9b
datcom-unibw <66484811+datcom-unibw@users.noreply.github.com>
2020-09-09 15:15:15 +0200

    add uni logo to welcome page background

--- app/features/welcome/styled/Wrapper.js
+++ app/features/welcome/styled/Wrapper.js
@@ -1,9 +1,14 @@
 // @flow
 
 import styled from 'styled-components';
+import uni_logo from '../../../images/uni-logo_schrift.png';
 
 export default styled.div`
-    background: #7F7F7F;
+    background: #909090;
+    background-image: url(${uni_logo});
+    background-position: 97% 3%;
+    background-repeat: no-repeat;
+    background-size: 20% auto;
     display: flex;
     flex-direction: column;
     height: 100vh;
