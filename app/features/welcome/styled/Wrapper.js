1359845f0bc067c026ff5c9b9dcf74646b341451
datcom-unibw <feedback@datcom-unibw.de>
2020-09-15 15:27:58 +0200

    Bump version to 2.3.201 and make uni_logo camel case.

--- app/features/welcome/styled/Wrapper.js
+++ app/features/welcome/styled/Wrapper.js
@@ -1,11 +1,11 @@
 // @flow
 
 import styled from 'styled-components';
-import uni_logo from '../../../images/uni-logo_schrift.png';
+import uniLogo from '../../../images/uni-logo_schrift.png';
 
 export default styled.div`
     background: #909090;
-    background-image: url(${uni_logo});
+    background-image: url(${uniLogo});
     background-position: 97% 3%;
     background-repeat: no-repeat;
     background-size: 20% auto;
